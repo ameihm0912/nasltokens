79887f27fdcae0687112437fdec3ea93f74164ca
Aaron Meihm <ameihm@mozilla.com>
2016-01-07 10:45:57 -0600

    indicate affected file on error

--- main.c
+++ main.c
@@ -9,12 +9,13 @@ extern char	*yytext;
 extern int	yyparse(void);
 
 struct parserstate	ps;
+char			*curfile = NULL;
 int 			lineno = 1;
 int			debug = 0;
 
 void	yyerror(const char *);
 int	yywrap(void);
-void	nparse(char *);
+void	nparse();
 void	usage(void);
 
 int
@@ -26,21 +27,22 @@ yywrap()
 void
 yyerror(const char *s)
 {
-	fprintf(stderr, "yyparse: %s: line %d\n", s, lineno);
+	fprintf(stderr, "yyparse: %s: %s: line %d\n", curfile, s, lineno);
 	exit(2);
 }
 
 void
-nparse(char *fp)
+nparse()
 {
 	FILE *f;
 
-	f = fopen(fp, "r");
+	f = fopen(curfile, "r");
 	if (f == NULL) {
 		perror("fopen");
 		exit(2);
 	}
 	yyin = f;
+	lineno = 1;
 	yyparse();
 	fclose(f);
 }
@@ -56,7 +58,6 @@ int
 main(int argc, char *argv[])
 {
 	char ch;
-	char *fp;
 	int i;
 
 	while ((ch = getopt(argc, argv, "dh")) != -1) {
@@ -77,9 +78,9 @@ main(int argc, char *argv[])
 
 	printf("{\n    \"vulnerabilities\": [\n");
 	for (i = 0; i < argc; i++) {
-		fp = argv[i];
+		curfile = argv[i];
 		memset(&ps, 0, sizeof(ps));
-		nparse(fp);
+		nparse(curfile);
 	}
 	printf("\n    ]\n}\n");
 
